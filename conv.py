dbe84864c482488f1b97ff5cd0d927bd70e461ff
Zachi225c <34230524+Zachi225c@users.noreply.github.com>
2019-10-23 13:03:54 +0200

    graf funkcji

--- conv.py
+++ conv.py
@@ -1,6 +1,8 @@
 import os
 import json
 
+
+df = "def"
 imp = "import"
 path = "/Users/marcinzachura/PycharmProjects/untitled/grafy/Grafy"
 files = []
@@ -10,6 +12,11 @@ hmap = {}
 counter = 0
 lines = ()
 lineCounter = 0
+
+func_names = []
+func_counter = 0
+func_dict = {}
+all_func_dict = {}
 
 def main():
     global lines
@@ -22,6 +29,8 @@ def main():
             saveToDictionary(fil)
             words = []
     saveToFile()
+    print(func_names)
+
 
 def getFiles():
     # r=root, d=directories, f = files
@@ -43,24 +52,40 @@ def getWords():
             word = list_of_words[1]
             if word is not "=":
                 words.append(word)
+        if df in line:
+            list_of_func_names = line.split();
+            word = list_of_func_names[1]
+            if word is not "=":
+                func_names.append(word)
     lineCounter = 0
 
 def saveToDictionary(fil):
     global counter
     global hmap
+    global func_dict
+    global func_counter
     for w in words:
         for line in lines:
             if str(w) in line:
                 counter = counter + 1
                 hmap[w] = counter
         counter = 0
+    for name in func_names:
+        for line in lines:
+            if str(name) in line:
+                func_counter = func_counter + 1
+                func_dict[name] = func_counter
+        func_counter = 0
     fhmap[str(fil)] = hmap
+    all_func_dict[str(fil)] = func_dict;
     hmap = {}
+    func_dict = {}
 
 
 def saveToFile():
     with open('data.json', 'w') as js_file:
         data = json.dump(fhmap, js_file)
-
+    with open('func.json', 'w') as js_file:
+        data = json.dump(all_func_dict, js_file,indent=2,)
 
 main()
--- graph2.py
+++ graph2.py
@@ -2,20 +2,25 @@ import json
 import pydot
 
 datastore = {}
-
-def main():
+fundata = {}
+def maing():
     global datastore
+    global fundata
     with open('data.json', 'r') as f:
         datastore = json.load(f)
 
+    with open('func.json', 'r') as f:
+        fundata = json.load(f)
+    drawGraph()
 
 def drawGraph():
     global datastore
+    global fundata
     graph1 = pydot.Dot(graph_type='digraph')
-    cluster_1=pydot.Cluster('graf',label='graph2', style="filled",color="white")
+    cluster_1=pydot.Cluster('graf',label='graph2.py', style="filled",color="white")
     cluster_1.add_node(pydot.Node('gr', label=datastore['graph2.py']['lines'],shape="circle", style="filled", fillcolor="white"))
 
-    cluster_2=pydot.Cluster('conv',label='conv', style="filled",color="white")
+    cluster_2=pydot.Cluster('conv',label='conv.py', style="filled",color="white")
     cluster_2.add_node(pydot.Node('co', label=datastore['conv.py']['lines'], shape="circle", style="filled", fillcolor="white"))
 
     cluster_3=pydot.Cluster('pyd',label='pydot', style="filled",color="white")
@@ -43,9 +48,43 @@ def drawGraph():
     graph1.add_edge(pydot.Edge("gr", "jso",label=datastore['graph2.py']['json'], fontsize="10.0"))
     graph1.add_edge(pydot.Edge("co", "djso",label='1', fontsize="10.0"))
     graph1.add_edge(pydot.Edge("djso", "gr",label='1', fontsize="10.0"))
+    graph1.add_edge(pydot.Edge("co", "gr",label='1', fontsize="10.0"))
 
     graph1.write_png('graf.png')
 
+    graph2= pydot.Dot(graph_type='digraph')
+    cluster_1=pydot.Cluster('',label='', style="filled",color="white")
+    cluster_1.add_node(pydot.Node('main', label='main',shape="circle", style="filled", fillcolor="white"))
 
-main()
-drawGraph()
+    cluster_2=pydot.Cluster('',label='', style="filled",color="white")
+    cluster_2.add_node(pydot.Node('getf', label='getFiles', shape="circle", style="filled", fillcolor="white"))
+
+    cluster_3=pydot.Cluster('',label='', style="filled",color="white")
+    cluster_3.add_node(pydot.Node('getw', label='getWords',shape="circle", style="filled", fillcolor="white"))
+
+    cluster_4=pydot.Cluster('',label='', style="filled",color="white")
+    cluster_4.add_node(pydot.Node('std', label='saveToDictionary', shape="circle", style="filled", fillcolor="white"))
+
+    cluster_5=pydot.Cluster('',label='', style="filled",color="white")
+    cluster_5.add_node(pydot.Node('stf', label='saveToFile', shape="circle", style="filled", fillcolor="white"))
+
+    cluster_6=pydot.Cluster('',label='', style="filled",color="white")
+    cluster_6.add_node(pydot.Node('dg', label='drawGraph', shape="circle", style="filled", fillcolor="white"))
+
+    graph2.add_subgraph(cluster_1)
+    graph2.add_subgraph(cluster_2)
+    graph2.add_subgraph(cluster_3)
+    graph2.add_subgraph(cluster_4)
+    graph2.add_subgraph(cluster_5)
+    graph2.add_subgraph(cluster_6)
+
+    graph2.add_edge(pydot.Edge("main", "dg",label='1', fontsize="10.0"))
+    graph2.add_edge(pydot.Edge("main", "stf",label='1', fontsize="10.0"))
+    graph2.add_edge(pydot.Edge("main", "std",label='1',  fontsize="10.0"))
+    graph2.add_edge(pydot.Edge("main", "getw",label='1', fontsize="10.0"))
+    graph2.add_edge(pydot.Edge("main", "getf",label='1', fontsize="10.0"))
+
+
+    graph2.write_png('graffunkcji.png')
+
+maing()
